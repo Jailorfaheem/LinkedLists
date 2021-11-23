a48c839d3a7de42a4075426eb02d1e0fac56076d
faheemjailor <faheemjailor0055>
2021-11-23 21:52:12 +0530

    Faheem UC2 for Linked List Operation

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace LinkedLists
+namespace LinkedList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //creating object object of LinkedList class
+            LinkedList list = new LinkedList();
+            //calling add method and provideing data
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //calling display method
+            list.Display();
+            list.AddFirst(70);
+            list.AddFirst(30);
+            list.AddFirst(56);
+            Console.ReadLine();
         }
     }
 }
